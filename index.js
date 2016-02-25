6aa5e693f7e0de7bbd80585e5762fc1f9459a9a1
Yu Qi <iyuq@outlook.com>
2016-02-25 08:45:27 +0800

    Add context to the promisfy.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var promisefy = require('./promisefy').promisefy;
 
-var readFile = promisefy(fs.readFile);
+var readFile = promisefy(fs.readFile, null);
 readFile('file1.txt', 'utf8').then(function (file1) {
     return readFile(file1.trim(), 'utf8');
 }).then(function (file2) {
